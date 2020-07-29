681055cfb71ad129ece4009c183e2d314d5c777d
Vasiliy <kkolyasev1228@gmail.com>
2020-07-29 14:51:22 +0300

    index

--- index.php
+++ index.php
@@ -1,57 +1,296 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package gradient
- */
-
-get_header();
-?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<!DOCTYPE html>
+<html lang="ru-Ru"></html>
+<head><meta charset="utf-8">
+    <title>Gradient.com</title>
+    <link rel="shortcut icon" href="./img/srdgnw.png" type="image/png">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ff2f3aab2f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light"><a class="navbar-brand" href="./index.html"><img class="logo" src="./img/logo.png" alt="logo"/></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="main-navbar">
+        <ul class="navbar-nav">
+            <li class="nav-item active"><a class="nav-link" href="./about.html">Мы</a></li>
+            <li class="nav-item"><a class="nav-link" href="./portfolio.html">Проекты</a></li>
+            <li class="nav-item"><a class="nav-link" href="./services.html">Услуги</a></li>
+            <li class="nav-item"><a class="nav-link" href="./blog.html">Блог</a></li>
+            <li class="nav-item"><a class="nav-link" href="./contacts.html">Контакты</a></li>
+            <button class="btn-togler activity ml-lg-5" type="submit">RU</button>
+            <button class="btn-togler" type="submit">EN</button>
+        </ul>
+    </div>
+</nav>
+<section class="main">
+    <div class="title-block"><img class="title-block__upper" src="./img/design.svg"/><img class="title-block__lower" src="./img/sites.svg"/><a class="beginning" href="#"><span class="beginning__text">Начать проект</span><img class="beginning__arrow" src="./img/arrow.svg"/></a></div>
+</section>
+<div class="carousel-block">
+    <div class="container">
+        <h1>Ведем проекты по четкому плану</h1>
+        <div class="row align-items-center">
+            <div class="col-lg-11">
+                <div class="carousel-cont">
+                    <div class="carousel-iterator">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="carousel-item__title">Цель</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h4 class="carousel-item__subtitle">Начинаем любую разработку с проработки целей</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <p class="carousel-item__description">Цель - важная составляющая любого проекта. Важно ответить на вопрос “Зачем?”, прежде чем думать “Как?”</p>
+                            </div>
+                        </div>
+                        <h1 class="carousel-item__number">1</h1>
+                    </div>
+                    <div class="carousel-iterator">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="carousel-item__title">Аналитика</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h4 class="carousel-item__subtitle">Поняли цель? Переходим к сбору данных</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <p class="carousel-item__description">Проводим всю необходимую для разработки аналитику. От рынка до пользовательского поведения.</p>
+                            </div>
+                        </div>
+                        <h1 class="carousel-item__number">2</h1>
+                    </div>
+                    <div class="carousel-iterator">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="carousel-item__title">Проектирование</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h4 class="carousel-item__subtitle">Собрав аналитику переходим к скелету</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <p class="carousel-item__description">Проектирование - скелет любой разработки, который помогает избежать многих проблем при дизайне.</p>
+                            </div>
+                        </div>
+                        <h1 class="carousel-item__number">3</h1>
+                    </div>
+                    <div class="carousel-iterator">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="carousel-item__title">Разработка</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h4 class="carousel-item__subtitle">Согласовав макет переходим к разработке</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <p class="carousel-item__description">Самый долгий и сложный этап, но он становится намного проще, если пройдены предыдущие шаги.</p>
+                            </div>
+                        </div>
+                        <h1 class="carousel-item__number">4</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <button class="next"><img class="next_img" src="./img/arrow_black.svg" alt="alt"/></button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="carousel-navigation container">
+    <p class="carousel-navigation_iterator active_iterator">Цель</p><img src="./img/arrow.svg" alt="arrow"/>
+    <p class="carousel-navigation_iterator">Аналитика</p><img src="./img/arrow.svg" alt="arrow"/>
+    <p class="carousel-navigation_iterator">Проектирование</p><img src="./img/arrow.svg" alt="arrow"/>
+    <p class="carousel-navigation_iterator">Разработка</p>
+</div>
+<section class="works-block container">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 p-0">
+            <div class="item item1"><span class="item__title">Презентация и сайт для EP Consult</span></div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 p-0">
+            <div class="item item2"><span class="item__title">Сайт для Венеция</span></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 p-0">
+            <div class="item item3"><span class="item__title">Презентация для Get Dent</span></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+            <div class="item-half"><span class="item__title">Сайт для Правил Роста</span></div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+            <div class="col-lg-12 col-md-12 col-sm-12 p-0">
+                <div class="item item4"><span class="item__title">Сайт для Авис</span></div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 p-0">
+                <div class="item item6"><span class="item__title">Фирменный стиль для Сфера 21 Век</span></div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-end"><a class="beginning mt-5 mb-5" href="#"><span class="beginning__text">Все работы</span><img class="beginning__arrow" src="./img/arrow.svg"/></a></div>
+</section>
+<section class="partners container-fluid">
+    <div class="container">
+        <div class="content">
+            <div class="row align-items-lg-center">
+                <div class="partners__text col-lg-5 col-md-12 col-12">
+                    <h1 class="partners__text__title">Нам доверяют</h1><span class="partners__text__subtitle">Мы работаем с малым и средним бизнесом <br/></span><span class="partners__text__subtitle">Наша специлизация - разработка корпоративных сайтов и быстрая упаковка бизнеса.</span>
+                </div>
+                <div class="partners__logotypes col-lg-7 col-12 justify-content-center">
+                    <div class="partners__logotypes__upper row"><img class="logotype" src="./img/logoGetDent.png" alt="logoGetDent"/><img class="logotype" src="./img/daEdu.png" alt="logodaEdu"/><img class="logotype" src="./img/saunMaster.png" alt="logoSaunMaster"/></div>
+                    <div class="partners__logotypes__middle row"><img class="logotype" src="./img/ideaClub.png" alt="logoIdeaClub"/><img class="logotype" src="./img/stomClub.png" alt="logoStomClub"/><img class="logotype" src="./img/mikeline.png" alt="logoMikeLine"/></div>
+                    <div class="partners__logotypes__lower row"><img class="logotype" src="./img/nobilis.png" alt="logoNobilis"/><img class="logotype" src="./img/sphere.png" alt="logoSphere"/><img class="logotype" src="./img/novostroy.png" alt="logoNovostroy"/></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="command-block">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="command-block__card"><img class="command-block__card__image" src="./img/alexandr.jpeg" alt="director-image"/>
+                    <h1 class="command-block__card__title">Александр</h1><span class="command-block__card__subtitle">Основатель, IT-teamleader</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="command-block__card"><img class="command-block__card__image" src="./img/vladislav.jpeg" alt="art-director"/>
+                    <h1 class="command-block__card__title">Владислав</h1><span class="command-block__card__subtitle">Основатель, дизайн-менеджер</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12"><a class="beginning" href="#"><span class="beginning__text">Вся команда</span><img class="beginning__arrow" src="./img/arrow.svg"/></a></div>
+        </div>
+    </div>
+</section>
+<section class="advantages">
+    <div class="advantages__first">
+        <h1 class="advantages-title">С нами</h1>
+        <div class="row align-items-center">
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="advantages__first__card">
+                    <h2 class="advantages-subtitle">Надежно</h2><span class="advantages-description">Мы несем отвественность за результат, все договорненности закрепляем юридически.</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="advantages__first__card">
+                    <h2 class="advantages-subtitle">Удобно</h2><span class="advantages-description">У нас выстроенные и понятные процессы работы. Мы всегда на связи и готовы отчитаться о каждом этапе.</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="advantages__first__card">
+                    <h2 class="advantages-subtitle">Легко</h2><span class="advantages-description">Мы всегда слушаем и слышим клиента и предлагаем решения, которые отвечаеют поставленным целям.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="advantages__second">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-sm-12">
+                    <div class="advantages__first__card">
+                        <h2 class="advantages__second__numbers">2</h2>
+                        <h3 class="advantages__second__subtitle">года на рынке</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12">
+                    <div class="advantages__first__card">
+                        <h2 class="advantages__second__numbers">>40</h2>
+                        <h3 class="advantages__second__subtitle">проектов реализовано</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12">
+                    <div class="advantages__first__card">
+                        <h2 class="advantages__second__numbers">90%</h2>
+                        <h3 class="advantages__second__subtitle">клиентов возвращаются снова</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container"><span class="question">   Остались вопросы? Напишите нами</span>
+        <div class="row align-items-center mb-3 justify-content-between">
+            <div class="col-lg-7 col-md-12 col-sm-12">
+                <h1 class="email"> go@thegradient.ru</h1>
+            </div>
+            <div class="col-lg-auto col-md-12 col-sm-12">
+                <input class="begin-project" type="submit" name="agreement" value="Начать проект"/>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="row mt-3 justify-content-between">
+            <div class="col-lg-4 col-6">
+                <p class="d-block">САНКТ-ПЕТЕРБУРГ, ТИХОРЕЦКИЙ 17Б</p>
+            </div>
+            <div class="col-lg-2 col-4">
+                <div class="socials-block d-flex align-items-center"><a class="socials-href" href="#"><img class="socials one" src="./img/behance.png" alt="behance_link"/></a><a class="socials-href" href="#"><img class="socials two" src="./img/instagram.png" alt="behance_link"/></a></div>
+            </div>
+        </div>
+        <div class="row footer__bottom">
+            <div class="col-lg-3 col-4 col-6">
+                <ul>
+                    <li class="footer-nav"><a href="./about.html">О студии</a></li>
+                    <li class="footer-nav"><a href="./portfolio.html">Портфолио</a></li>
+                    <li class="footer-nav"><a href="./services.html">Услуги</a></li>
+                    <li class="footer-nav"><a href="./blog.html">Блог</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-6">
+                <ul>
+                    <li class="footer-nav"><a href="">Сайты</a></li>
+                    <li class="footer-nav"><a href="">Фирменный стиль</a></li>
+                    <li class="footer-nav"><a href="">Презентации</a></li>
+                    <li class="footer-nav"><a href="">Пакетные услуги</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul class="cases">
+                    <li class="footer-nav"><a href="">Кейс 1</a></li>
+                    <li class="footer-nav"><a href="">Кейс 2</a></li>
+                    <li class="footer-nav"><a href="">Кейс 3</a></li>
+                    <li class="footer-nav"><a href="">Кейс 4</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col md-12 col-sm-12 col-12">
+                <form class="form_activation" action="/login" method="POST">
+                    <div class="row justify-content-center justify-content-lg-start">
+                        <div class="col-6 col-lg-10 p-0">
+                            <h3 class="form__title">Будем присылать вам интересные материалы</h3>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center justify-content-lg-start">
+                        <div class="col-lg-9 col-md-8 col-6 p-0">
+                            <input class="footer_email" type="text" name="username" placeholder="Enter Your Username "/>
+                        </div>
+                        <div class="col-lg-3 col-md-8 col-1 p-0">
+                            <button class="form__submit" type="submit"><img src="./img/arrow_black.svg"/></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</footer><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="./js/index.js"></script>
+</body>
