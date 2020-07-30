1c93239d9f72dc9afd52d7f44a98d4b275ae0169
Vasiliy <kkolyasev1228@gmail.com>
2020-07-30 20:29:06 +0300

    list

---
+++ framework-customizations/extensions/shortcodes/shortcodes/list/options.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$options = array(
+	'div_class'    => array(
+		'type'  => 'text',
+		'label' => __( 'Div Class', 'fw' ),
+		'desc'  => __( 'Div Class', 'fw' ),
+	)
+);
---
+++ framework-customizations/extensions/shortcodes/shortcodes/list/views/view.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+?>
+<div class="<?=$atts['div_class']?>"></div>
+
